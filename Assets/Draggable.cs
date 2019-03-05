800f991495ef97860e86700fce015224668a19c2
manygames <weberdls@gmail.com>
2019-03-05 15:08:52 -0300

    drophandler

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -3,21 +3,23 @@ using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
     
-    private Transform parentToReturnTo = null;
+    public Transform parentToReturnTo = null;
     
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("OnBeginDrag");
         parentToReturnTo = transform.parent;
-        this.transform.SetParent(this.transform.parent.parent);
+        transform.SetParent(transform.parent.parent);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData) {
-        Debug.Log("OnDrag");
+        //Debug.Log("OnDrag");
         transform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log("OnEndDrag");
         transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 }
---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler 
+{
+    public void OnDrop(PointerEventData eventData) {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        var draggedItem = eventData.pointerDrag.GetComponent<Draggable>();
+        if (draggedItem != null) {
+            draggedItem.parentToReturnTo = this.transform;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        //Debug.Log("OnPointerEnter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        //Debug.Log("OnPointerExit");
+    }
+}
